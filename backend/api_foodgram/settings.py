348601d3f2273b3126401068bf5aab01368bba71
xaer981 <xaer981@yandex.ru>
2023-05-19 15:44:02 +0300

    added logging to django settings

--- backend/api_foodgram/settings.py
+++ backend/api_foodgram/settings.py
@@ -140,3 +140,23 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
