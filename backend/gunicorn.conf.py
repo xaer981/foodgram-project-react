04a615c68a16730188fc6a42c77611f4c31c00b4
xaer981 <xaer981@yandex.ru>
2023-05-19 14:35:02 +0300

    fixing errorlogs and accesslogs path in gunicorn config

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,5 +1,5 @@
 bind = "0:8000"
-accesslog = "var/log/gunicorn.access.log"
-errorlog = "var/log/gunicorn.error.log"
+accesslog = "../var/log/gunicorn.access.log"
+errorlog = "../var/log/gunicorn.error.log"
 capture_output = True
 loglevel = "info"
